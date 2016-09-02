5b3b765028322954c1e9252d40155810c9742225
FTT\kimsin <kimsin3003@naver.com>
2016-09-02 18:31:41 +0900

    Let's make shder clas.

--- RainyDay/RainyDay/Shader.h
+++ RainyDay/RainyDay/Shader.h
@@ -4,5 +4,13 @@ class Shader
 public:
 	Shader();
 	~Shader();
+
+	
+
+private:
+	ID3DX11Effect*									m_effect = nullptr;
+	ID3DX11EffectTechnique*							m_colorTech = nullptr;
+	
+	D3DX11_PASS_DESC m_passDesc;
 };
 
